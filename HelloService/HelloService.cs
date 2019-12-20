bf94657f0f8925246cd07459d6fab8070d53da3c
Nox Wang <nox.gr.wang@gmail.com>
2019-12-20 10:33:39 +0800

    Initail version

---
+++ HelloService/HelloService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CINugetSample
+{
+    public class HelloService
+    {
+        public string SayHello()
+        {
+            return "Hello!";
+        }
+    }
+}
